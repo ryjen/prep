f9b0fddbc45fbe5ababb3cb7c4eee2ed57957a6e
RJ <rybertjen@gmail.com>
2026-08-13 02:42:14 -0700

    test: assert failed plugin process is typed

--- crates/prep-test-support/tests/protocol_smoke.rs
+++ crates/prep-test-support/tests/protocol_smoke.rs
@@ -32,3 +32,20 @@ fn malformed_external_plugin_output_is_typed_failure() {
         CliError::Probe(PluginProbeError::Protocol(_))
     ));
 }
+
+#[test]
+fn unsuccessful_plugin_process_is_typed_failure() {
+    let plugin = env!("CARGO_BIN_EXE_prep-crash-plugin").to_owned();
+    let error = run_from(vec![
+        "prep".to_owned(),
+        "internal".to_owned(),
+        "probe-plugin".to_owned(),
+        plugin,
+    ])
+    .expect_err("non-zero plugin exit should fail");
+
+    assert!(matches!(
+        error,
+        CliError::Probe(PluginProbeError::ProcessFailed(Some(17)))
+    ));
+}
